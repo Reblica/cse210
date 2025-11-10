352f7a2a7b25a1f5e1bfb3daf81bab36c87e915e
Reblica <dean.thomas.hibbs@gmail.com>
2025-11-10 17:16:42 -0600

    Added Job class to print job title, company, start year, and end year, as well as created Resume file which creates a file of each job and prints them out, including name.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Lead Engineer";
+        job1._startYear = 2024;
+        job1._endYear = 2030;
+
+        Job job2 = new Job();
+        job2._company = "Sony";
+        job2._jobTitle = "Head Designer";
+        job2._startYear = 2012;
+        job2._endYear = 2024;
+
+        job1.Display();
+        job2.Display();
+
+        Resume myResume = new Resume();
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
