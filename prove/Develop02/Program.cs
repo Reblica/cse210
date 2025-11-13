4b9dcea37a45eaa37dc8513556805404b9bc290c
Reblica <dean.thomas.hibbs@gmail.com>
2025-11-12 22:13:17 -0600

    Began framework for journal menu and functions

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,60 @@
 using System;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Journal journal = new Journal();
+        string choice = "";
+
+        Console.WriteLine("Welcome to the Journal Age!");
+
+        while (choice != "5")
+        {
+            Console.WriteLine("\nMenu");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+
+            choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+            
+            }
+            else if (choice == "2")
+            {
+
+            }
+            else if (choice == "3")
+            {
+                
+            }
+            else if (choice == "4")
+            {
+                
+            }
+            
+            
+
+
+    
+        }
     }
+    public void SaveToFile()
+    {
+        
+    }
+
+    public void LoadToFile()
+    {
+        
+    }
+    
+        
+    
 }
