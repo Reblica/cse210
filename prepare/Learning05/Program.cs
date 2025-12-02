cb5c5918cfa73afe6acba89f51d083e2cb8973b7
Reblica <dean.thomas.hibbs@gmail.com>
2025-12-02 13:21:30 -0600

    Created a program which finds the area of a square, rectangle and circle.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Text.RegularExpressions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square sq = new Square("Blue", 2);
+        shapes.Add(sq);
+
+        Rectangle re = new Rectangle("Yellow", 3, 6);
+        shapes.Add(re);
+
+        Circle ci = new Circle("Green", 7);
+        shapes.Add(ci);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape Color: {shape.GetColor()}");
+            Console.WriteLine($"Shape Area: {shape.GetArea()}");
+            Console.WriteLine();
+        }
     }
 }
